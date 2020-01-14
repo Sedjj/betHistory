4eda4fcbb46b9b99c9cc1613a29f948cc09694a5
Sedjj <omeldarl@rambler.ru>
2020-01-14 18:10:22 +0500

    убрал коментарии

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,10 @@
 import {Get, Controller, Res, HttpStatus} from '@nestjs/common';
-/*import {BotService} from './telegram/bot/bot.service';*/
 import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
 	constructor(
 		private readonly appService: AppService,
-		/*private botService: BotService*/
 	) {
 	}
 
