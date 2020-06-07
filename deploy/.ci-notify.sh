03a84b3f326276f831856d6f805c00adb64b822f
Sedjj <omeldarl@rambler.ru>
2020-06-07 21:14:50 +0500

    поправил деплой сервера

--- deploy/.ci-notify.sh
+++ deploy/.ci-notify.sh
@@ -4,4 +4,4 @@ TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Status: $1%0ABranch:+$CI_COMMIT_REF_SLUG%0AAuthor:+$GITLAB_USER_NAME"
 
-curl -s --socks5-hostname $TELEGRAM_PROXY -U $TELEGRAM_PROXY_USERPASSWORD --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=true" $URL > /dev/null
+curl --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=true" $URL > /dev/null
