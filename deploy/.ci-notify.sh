9b7ea35be14a56d6e1139140e2f37c3b4fc72a39
Sedjj <omeldarl@rambler.ru>
2020-06-10 20:41:19 +0500

    прол

--- deploy/.ci-notify.sh
+++ deploy/.ci-notify.sh
@@ -4,4 +4,4 @@ TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Status: $1%0ABranch:+$CI_COMMIT_REF_SLUG%0AAuthor:+$GITLAB_USER_NAME"
 
-curl --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=true" $URL > /dev/null
+sudo curl --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=true" $URL > /dev/null
