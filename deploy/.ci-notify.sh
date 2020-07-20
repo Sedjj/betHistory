a9510d937830cbe5c8b9fe1f1b0bbb8120a9b823
DESKTOP-6BAT9SP\Sedjj <sedjjon@gmail.com>
2020-07-20 21:55:50 +0500

    поправил нотификиции пересборки

--- deploy/.ci-notify.sh
+++ deploy/.ci-notify.sh
@@ -2,6 +2,6 @@
 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Status: $1%0ABranch:+$CI_COMMIT_REF_SLUG%0AAuthor:+$GITLAB_USER_NAME"
+TEXT="Status: $1%0AProject:+$CI_PROJECT_NAME%0ABranch:+$CI_COMMIT_REF_SLUG%0AAuthor:+$GITLAB_USER_NAME"
 
 curl --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=true" $URL > /dev/null
