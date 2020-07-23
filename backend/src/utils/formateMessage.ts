be0d1794d5f7bbc6ca00d289c4c7597f81a3cabd
DESKTOP-6BAT9SP\Sedjj <sedjjon@gmail.com>
2020-07-23 23:00:12 +0500

    добавил ссылка из чата на другой сайт

--- backend/src/utils/formateMessage.ts
+++ backend/src/utils/formateMessage.ts
@@ -25,7 +25,8 @@ export function decorateMessageChannel(param: IFootball): string {
 	const scope = `${sc1}:${sc2}`;
 	const index = `${p1} / ${x} / ${p2}`;
 	const difference = `${TM15} / ${TM25} / ${OZ}:НЕТ`;
-	let mobile = `<a href="https://www.betfair.com/exchange/football/event/${eventId}/multi-market?marketIds${marketId}">mobile</a>`;
-	let pc = `<a href="https://www.betfair.com/exchange/plus/football/market/${marketId}">pc</a>`;
-	return `${mobile} / ${pc}\n${group}\n\n<b>${one}\n${two}</b>\n\n<pre>${scope} / ${time}'\n${index}\n${difference}</pre>`;
+	const mobile = `<a href="https://www.betfair.com/exchange/football/event/${eventId}/multi-market?marketIds${marketId}">mobile</a>`;
+	const pc = `<a href="https://www.betfair.com/exchange/plus/football/market/${marketId}">pc</a>`;
+	const orbitx = `<a href="https://www.orbitExch.com/customer/sport/market/${marketId}">pc</a>`;
+	return `${mobile} / ${pc} / ${orbitx}\n${group}\n\n<b>${one}\n${two}</b>\n\n<pre>${scope} / ${time}'\n${index}\n${difference}</pre>`;
 }
