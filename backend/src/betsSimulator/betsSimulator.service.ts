fe48bbd2be61422d08c540609be2c9a4c3d23a10
DESKTOP-6BAT9SP\Sedjj <sedjjon@gmail.com>
2021-05-29 18:55:08 +0500

    ор

--- backend/src/betsSimulator/betsSimulator.service.ts
+++ backend/src/betsSimulator/betsSimulator.service.ts
@@ -1,17 +1,26 @@
 import {Injectable} from '@nestjs/common';
 import {IFootball} from '../model/football/type/football.type';
 import {TelegramService} from '../telegram/telegram.service';
-import {decorateMessageChat} from '../utils/formateMessage';
+import {decorateMessageChannel, decorateMessageChat} from '../utils/formateMessage';
 import {betAmount} from '../store';
 import {FetchService} from '../fetch/fetch.service';
 
 @Injectable()
 export class BetsSimulatorService {
-	/*private groupForChannel: string[];*/
+	private groupForChannel: string[];
 	private groupForRate: string[];
 
 	constructor(private readonly telegramService: TelegramService, private readonly fetchService: FetchService) {
-		/*this.groupForChannel = [];*/
+		this.groupForChannel = [
+			'Panamanian',
+			'Jordanian',
+			'Israeli',
+			'Hungarian NB II',
+			'Greek Super League 2',
+			'Georgian',
+			'Ecuadorian Serie A',
+			'Argentinian Primera',
+		];
 		this.groupForRate = [
 			'AFC',
 			'Argentinian',
@@ -82,7 +91,11 @@ export class BetsSimulatorService {
 					over: {against: TB15A, selectionId: over15SelectionId, handicap: over15Handicap},
 					under: {behind: TM15B, selectionId: under15SelectionId, handicap: under15Handicap},
 				},
-				/*goalLines: {list},*/
+				goalLines: {list},
+				bothTeamsToScore: {
+					yes: {behind: bothYes},
+					no: {behind: bothNo},
+				},
 			},
 			cards: {
 				one: {corners: cornersOne},
@@ -91,16 +104,28 @@ export class BetsSimulatorService {
 			command: {group, youth, women},
 		} = param;
 
-		/*const TM20 = list.reduce<number>((acc, x) => {
+		const TM20 = list.reduce<number>((acc, x) => {
 			if (x.under.handicap === 2.0 || x.under.handicap === 2) {
 				acc = x.under.behind;
 			}
 			return acc;
-		}, 0);*/
-		// const excludeGroupChannel = this.groupForChannel.some(x => group.includes(x));
+		}, 0);
+		const excludeGroupChannel = this.groupForChannel.some(x => group.includes(x));
 		const excludeGroupRate = this.groupForRate.some(x => group.includes(x));
 
 		switch (param.strategy) {
+			case 2:
+				if (!excludeGroupChannel) {
+					if (women === 0 && 2.4 <= TM20) {
+						if (1.1 <= bothNo && bothNo <= 2.2) {
+							if (bothYes < 1.9) {
+								await this.telegramService.sendMessageChannel(decorateMessageChannel(param));
+								await this.telegramService.sendMessageChannel('ТБ2.5');
+							}
+						}
+					}
+				}
+				break;
 			case 4:
 				if (!excludeGroupRate) {
 					if (women === 0 && youth === 0) {
