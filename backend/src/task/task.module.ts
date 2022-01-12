cfda4498bd849ed6c0702ed93bf0aefa6df64bb6
DESKTOP-6BAT9SP\Sedjj <sedjjon@gmail.com>
2022-01-12 20:53:07 +0500

    гншнгш

--- backend/src/task/task.module.ts
+++ backend/src/task/task.module.ts
@@ -3,11 +3,10 @@ import {DataAnalysisModule} from '../dataAnalysis/dataAnalysis.module';
 import {ScheduleModule} from '@nestjs/schedule';
 import {TaskService} from './task.service';
 import {ParserFootballModule} from '../parser/parserFootball.modul';
-import {QueueModule} from './queue/queue.module';
 import {StackModule} from './stack/stack.module';
 
 @Module({
-	imports: [ScheduleModule.forRoot(), DataAnalysisModule, ParserFootballModule, StackModule, QueueModule],
+	imports: [ScheduleModule.forRoot(), DataAnalysisModule, ParserFootballModule, StackModule],
 	providers: [TaskService],
 })
 export class TaskModule {}
