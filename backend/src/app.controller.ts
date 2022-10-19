3bdb6c42d87d41fec9837d0d85ad40bf74c7c1a8
Eldar <sedjjon@gmail.com>
2022-10-19 16:11:57 +0400

    фы

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Get, Controller, Res, HttpStatus} from '@nestjs/common';
+import {Controller, Get, HttpStatus, Res} from '@nestjs/common';
 import {AppService} from './app.service';
 
 @Controller()
@@ -12,6 +12,6 @@ export class AppController {
 
 	@Get()
 	getBotDialog(@Res() res: any) {
-		res.status(HttpStatus.OK).send('Bot service started');
+		res.status(HttpStatus.OK).send('Bot service started!');
 	}
 }
